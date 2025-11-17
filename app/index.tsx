f012bf1b64aa0b873a691dda43110d3f4a0f7511
Fraser Patrick <fraserp2004@gmail.com>
2025-11-17 18:49:07 +0000

    Logout button

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,29 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
+import { Button } from 'react-native';
 import HomeScreen from './../screens/HomeScreen';
 import LoginScreen from './../screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 function ScreenNav() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
-  );
+    const handleLogout = (navigation) => {
+        alert('Logged out!');
+        navigation.navigate('Login');
+    };
+
+    return (
+        <Stack.Navigator screenOptions={({ navigation }) => ({headerRight: () => (
+                    <Button title="Logout" onPress={() => handleLogout(navigation)}/>
+                ),})}>
+            <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
+            <Stack.Screen name="Home" component={HomeScreen}/>
+        </Stack.Navigator>
+    );
 }
 
 export default function Index() {
-  return (
-      <ScreenNav/>
-  );
+    return (
+        <ScreenNav/>
+    );
 }
