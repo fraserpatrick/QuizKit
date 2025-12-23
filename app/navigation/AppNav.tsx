c3a084b3ba1af2070ae0faac4bee487647cf6ac5
Fraser Patrick <fraserp2004@gmail.com>
2025-12-23 18:38:47 +0000

    Removed admin screen. Created initial profile screen

--- app/navigation/AppNav.tsx
+++ app/navigation/AppNav.tsx
@@ -2,35 +2,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import QuizCreationScreen from '../screens/quizScreens/QuizCreationScreen';
 import QuizEditor from '../screens/quizScreens/QuizEditor';
-import AdminScreen from '../screens/AdminScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 import QuizInfoScreen from '../screens/quizScreens/QuizInfoScreen';
 import { Button } from 'react-native';
-import { useAuth } from "@/app/AuthContext";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNav() {
-    const { logout } = useAuth();
 
     const homeScreenOptions = ({navigation}) => ({
         title: 'QuizKit',
         headerRight: () => (
-            <Button title="Logout" onPress={() => handleLogout()} />
-        ),
-        headerLeft: () => (
-            <Button title="ADMIN" onPress={() => handleAdmin(navigation)} />
-        ),
+            <Button title="Profile" onPress={() => navigation.navigate('ProfileScreen' as never)} />
+        )
     });
-
-    const handleLogout = () => {
-        console.log('Logout button pressed');
-        alert('Logging out...');
-        logout();
-    }
-    const handleAdmin = (navigation) => {
-        navigation.navigate('AdminScreen' as never);
-    }
-
 
 
     return (
@@ -39,7 +24,7 @@ export default function AppNav() {
             <Stack.Screen name="QuizCreationScreen" component={QuizCreationScreen} />
             <Stack.Screen name="QuizInfoScreen" component={QuizInfoScreen} />
             <Stack.Screen name="QuizEditor" component={QuizEditor} />
-            <Stack.Screen name="AdminScreen" component={AdminScreen} />
+            <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
         </Stack.Navigator>
     );
 }
---
+++ app/screens/ProfileScreen.tsx
@@ -0,0 +1,75 @@
+import { Alert, Button, View } from 'react-native';
+import { useAuth } from '@/app/AuthContext';
+import database, { Quiz, User } from '@/DatabaseController';
+import { useState, useEffect } from 'react';
+
+export default function HomeScreen() {
+    const { logout } = useAuth();
+    const [quizzes, setQuizzes] = useState<Quiz[]>([]);
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        const loadQuizzes = async () => {
+            try {
+                setQuizzes(await database.getQuizzes());
+            } catch (error) {
+                console.error('Error loading quizzes:', error);
+            }
+        };
+        const loadUsers = async () => {
+            try {
+                setUsers(await database.getUsers());
+            } catch (error) {
+                console.error('Error loading users:', error);
+            }
+        };
+
+        loadQuizzes();
+        loadUsers();
+    }, []);
+
+    const handleLogout = () => Alert.alert(
+            'Logout', 'Are you sure you want to logout?', [
+                {text: 'No, stay logged in', style: 'cancel',},
+                {text: 'Yes, logout', onPress: () => logout(), style: 'destructive',},
+    ]);
+
+    const listDatabase = async () => {
+        if (quizzes.length === 0) {
+            console.log("No quizzes found");
+        } else {
+            console.log("Quizzes:");
+            quizzes.forEach((quiz) => {
+                console.log("ID:" + quiz.id + "  Name: " + quiz.name + "   UserID: " + quiz.userID + "   Visibility: " + quiz.visibility);
+            });
+        }
+        if (users.length === 0) {
+            console.log("No users found");
+        } else {
+            console.log("Users:");
+            users.forEach((user) => {
+                console.log("ID:" + user.id + "  Email: " + user.email + "   Username: " + user.username);
+            });
+        }
+    };
+
+    const resetDatabase = async () => {
+            try {
+                await database.databaseReset();
+                alert('Database has been reset.');
+                console.log('Database reset successfully.');
+            } catch (error) {
+                console.error('Error resetting database:', error);
+                alert('Failed to reset database.');
+            }
+        };
+
+
+    return (
+        <View>
+            <Button title="Logout" onPress={handleLogout} />
+            <Button title="Reset Database" onPress={resetDatabase} />
+            <Button title="List Database" onPress={listDatabase} />
+        </View>
+    );
+}
