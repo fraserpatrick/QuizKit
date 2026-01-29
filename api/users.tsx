0f758d23328ff9fcd82a1cde71bdf603e33ab74f
Fraser Patrick <fraserp2004@gmail.com>
2026-01-29 21:46:40 +0000

    Added missing returns

--- api/users.tsx
+++ api/users.tsx
@@ -2,23 +2,23 @@ import {apiFetch} from '@/api/apiConnection';
 
 
 export const createUser = (email: string, username: string) => {
-    apiFetch("users", "POST", {email, username});
+    return apiFetch("users", "POST", {email, username});
 }
 
 
 export const getUsers = (params = {}) => {
-    apiFetch("users", "GET", null, params);
+    return apiFetch("users", "GET", null, params);
 }
 
 export const getUser = (search: string) => {
-    getUsers(search);
+    return getUsers(search);
 }
 
 
 export const updateUsername = (email: string, newUsername: string) => {
-    apiFetch("users/updateUsername", "PUT", {email, newUsername});
+    return apiFetch("users/updateUsername", "PUT", {email, newUsername});
 }
 
 export const updateStats = (username: string, answered: number, correct: number) => {
-    apiFetch("users/updateStats", "PUT", {username, answered, correct});
+    return apiFetch("users/updateStats", "PUT", {username, answered, correct});
 }
