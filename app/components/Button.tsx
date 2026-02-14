415ba38cd1c67206fc0540440a8fc153cf1b779a
Fraser Patrick <fraserp2004@gmail.com>
2026-02-14 21:20:44 +0000

    Added fanfare and click, made all buttons click

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import { useSounds } from "../hooks/useSounds";
+
+const { playClick } = useSounds();
 
 interface ButtonProps {
     label: string;
@@ -8,7 +11,7 @@ interface ButtonProps {
 
 const PrimaryButton: React.FC<ButtonProps>= ({label, onPress}) => {
     return (
-        <TouchableOpacity onPress={onPress}>
+        <TouchableOpacity onPress={() => { playClick(); onPress(); }}>
             <View style={styles.button}>
                 <Text style={styles.buttonText}>{label}</Text>
             </View>
--- app/hooks/useSounds.tsx
+++ app/hooks/useSounds.tsx
@@ -3,10 +3,18 @@ import { AudioPlayer, createAudioPlayer } from 'expo-audio';
 
 export const useSounds = () => {
     const notification = useRef<AudioPlayer | null>(null);
+    const click = useRef<AudioPlayer | null>(null);
+    const fanfare = useRef<AudioPlayer | null>(null);
 
     useEffect(() => {
         notification.current = createAudioPlayer(
             require('../../assets/sounds/notification.mp3')
+        );
+        click.current = createAudioPlayer(
+            require('../../assets/sounds/mouse-click.mp3')
+        );
+        fanfare.current = createAudioPlayer(
+            require('../../assets/sounds/success-fanfare.mp3')
         );
     }, []);
 
@@ -14,7 +22,18 @@ export const useSounds = () => {
         notification.current?.seekTo(0);
         notification.current?.play();
     };
+    const playClick = () => {
+        click.current?.seekTo(0);
+        click.current?.play();
+    };
+    const playFanfare = () => {
+        fanfare.current?.seekTo(0);
+        fanfare.current?.play();
+    };
+
     return {
-        playNotification
+        playNotification,
+        playClick,
+        playFanfare
     };
 };
