54d077f52811e0f6b894bcbeca2c54aaeecca6ef
Fraser Patrick <fraserp2004@gmail.com>
2026-02-22 16:38:50 +0000

    Refactor local questions

--- localDatabase/questions.tsx
+++ localDatabase/questions.tsx
@@ -1,41 +1,53 @@
-import localDatabaseController from '@/localDatabase/localDatabaseController';
+import { db } from '@/localDatabase/databaseConnection';
 import { Question } from '@/app/components/Interfaces';
 
-class questionTable {
-    public createQuestion(quizID: number, type: string, text: string, correctAnswer: string, options: string[]): Promise<boolean> {
-        const insertSQL = `INSERT INTO question (quizID, type, text, correctAnswer, options) VALUES (?, ?, ?, ?, ?)`;
-        return localDatabaseController.execute(insertSQL, [quizID, type, text, correctAnswer, JSON.stringify(options)]);
-    }
+export const createLocalQuestion = async (quizID: number, type: string, text: string, correctAnswer: string, options: string[], feedback: string): Promise<boolean> => {
+    const insertSQL = `INSERT INTO question (quizID, type, text, correctAnswer, options, feedback) VALUES (?, ?, ?, ?, ?, ?)`;
+    const data = [quizID, type, text, correctAnswer, JSON.stringify(options), feedback];
+    await db.runAsync(insertSQL, data);
 
-    public async getQuestions(): Promise<Question[]> {
-        const sql = `SELECT * FROM question`;
-        const rows = await localDatabaseController.select<any>(sql);
-
-        return rows.map(row => ({
-            ...row,
-            options: row.options ? JSON.parse(row.options) : [],
-        }));
-    }
-
-    public async getQuestionsByQuizID(quizID: number): Promise<Question[]> {
-        const sql = `SELECT * FROM question WHERE quizID = ?`;
-        const rows = await localDatabaseController.select<any>(sql, [quizID]);
-        
-        return rows.map(row => ({
-            ...row,
-            options: row.options ? JSON.parse(row.options) : [],
-        }));
-    }
-
-    public updateQuestion(questionID: number, type: string, text: string, correctAnswer: string, options: string[]): Promise<boolean> {
-        const sql = `UPDATE question SET type = ?, text = ?, correctAnswer = ?, options = ? WHERE id = ?`;
-        return localDatabaseController.execute(sql, [type, text, correctAnswer, JSON.stringify(options), questionID]);
-    }
-
-    public deleteQuestion(questionID: number): Promise<boolean> {
-        const sql = `DELETE FROM question WHERE id = ?`;
-        return localDatabaseController.execute(sql, [questionID]);
-    }
+    return true;
 }
 
-export default new questionTable();
+export const getLocalQuestions = async (): Promise<Question[]> => {
+    const sql = `SELECT * FROM question`;
+    const result = db.getAllSync(sql);
+
+    return result.map((question: any) => ({
+        ...question,
+        options: JSON.parse(question.options),
+    })) as Question[];
+}
+
+export const getLocalQuizQuestions = async (quizID: number): Promise<Question[]> => {
+    const sql = `SELECT * FROM question WHERE quizID = ?`;
+    const result = db.getAllSync(sql, [quizID]);
+
+    return result.map((question: any) => ({
+        ...question,
+        options: JSON.parse(question.options),
+    })) as Question[];
+}
+
+export const updateLocalQuestion = async (questionID: number, text: string, type: string, correctAnswer: string, options: string[], feedback: string): Promise<boolean> => {
+    const sql = `UPDATE question SET text = ?, type = ?, correctAnswer = ?, options = ?, feedback = ? WHERE id = ?`;
+    const data = [type, text, correctAnswer, JSON.stringify(options), feedback, questionID];
+
+    await db.runAsync(sql, data);
+
+    return true;
+}
+
+export const deleteLocalQuestion = async (questionID: number): Promise<boolean> => {
+    const sql = `DELETE FROM question WHERE id = ?`;
+    await db.runAsync(sql, [questionID]);
+
+    return true;
+}
+
+export const deleteLocalQuestions = async (quizID: number): Promise<boolean> => {
+    const sql = `DELETE FROM question WHERE quizID = ?`;
+    await db.runAsync(sql, [quizID]);
+
+    return true;
+}
