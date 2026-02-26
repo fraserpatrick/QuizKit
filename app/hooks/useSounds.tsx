a29c325345da01a760752f0a4eaaaaad7293f188
Fraser Patrick <fraserp2004@gmail.com>
2026-02-26 14:11:26 +0000

    Fixed audio issues

--- app/hooks/useSounds.tsx
+++ app/hooks/useSounds.tsx
@@ -1,36 +1,17 @@
-import { useEffect, useRef } from 'react';
-import { AudioPlayer, createAudioPlayer } from 'expo-audio';
+import { notificationPlayer, clickPlayer, fanfarePlayer } from "@/app/utils/audioManager";
 
 export const useSounds = () => {
-    const notification = useRef<AudioPlayer | null>(null);
-    const click = useRef<AudioPlayer | null>(null);
-    const fanfare = useRef<AudioPlayer | null>(null);
-
-    useEffect(() => {
-        notification.current = createAudioPlayer(
-            require('../../assets/sounds/notification.mp3')
-        );
-        click.current = createAudioPlayer(
-            require('../../assets/sounds/mouse-click.mp3')
-        );
-
-        click.current.volume = 0.1;
-        fanfare.current = createAudioPlayer(
-            require('../../assets/sounds/success-fanfare.mp3')
-        );
-    }, []);
-
     const playNotification = () => {
-        notification.current?.seekTo(0);
-        notification.current?.play();
+        notificationPlayer.seekTo(0);
+        notificationPlayer.play();
     };
     const playClick = () => {
-        click.current?.seekTo(0);
-        click.current?.play();
+        clickPlayer.seekTo(0);
+        clickPlayer.play();
     };
     const playFanfare = () => {
-        fanfare.current?.seekTo(0);
-        fanfare.current?.play();
+        fanfarePlayer.seekTo(0);
+        fanfarePlayer.play();
     };
 
     return {
---
+++ app/utils/audioManager.tsx
@@ -0,0 +1,15 @@
+import { createAudioPlayer } from "expo-audio";
+
+export const notificationPlayer = createAudioPlayer(
+    require("../../assets/sounds/notification.mp3")
+);
+
+export const clickPlayer = createAudioPlayer(
+    require("../../assets/sounds/mouse-click.mp3")
+);
+
+export const fanfarePlayer = createAudioPlayer(
+    require("../../assets/sounds/success-fanfare.mp3")
+);
+
+clickPlayer.volume = 0.1;
